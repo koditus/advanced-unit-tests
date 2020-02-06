09ff55df5f6b01b8f88b955680829d1aeaed1e8a
Ignas Smirinenka <ignas.smirinenka@gmail.com>
2020-02-07 00:13:18 +0200

    [Unit test attributes] Few verifies;

--- UnitTests/ExampleOne.cs
+++ UnitTests/ExampleOne.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Moq;
 using NUnit.Framework;
 
 namespace UnitTests
@@ -18,6 +19,9 @@ namespace UnitTests
             Assert.IsNotNull(lending);
             Assert.AreEqual(ValidBookId, lending.Book.Id);
             Assert.AreEqual(ValidPersonId, lending.Person.Id);
+            MockBookRepository.Verify(mock => mock.GetOne(ValidBookId), Times.Once);
+            MockPersonRepository.Verify(mock => mock.GetOne(ValidPersonId), Times.Once);
+            MockLendingRepository.Verify(mock => mock.GetAll(), Times.Once);
         }
 
         [Test]
@@ -27,6 +31,9 @@ namespace UnitTests
 
             //Act / Assert
             Assert.Throws<InvalidOperationException>(() => LendingService.Lend(InvalidBookId, InvalidPersonId));
+            MockBookRepository.Verify(mock => mock.GetOne(InvalidBookId), Times.Once);
+            MockPersonRepository.Verify(mock => mock.GetOne(InvalidPersonId), Times.Never);
+            MockLendingRepository.Verify(mock => mock.GetAll(), Times.Never);
         }
     }
 }
